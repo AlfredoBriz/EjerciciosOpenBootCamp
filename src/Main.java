dfe6dd8a5dd0f3bd6758424f294d815592611735
AlfredoBriz <alfredobrizuela1@gmail.com>
2022-08-04 21:17:17 -0300

    Agrego Ejercicios OpenBootCamp

---
+++ src/Main.java
@@ -0,0 +1,90 @@
+//Para practicar la encapsulación:
+
+        //Crear clase Persona.
+
+        //Crear variables las privadas edad, nombre y telefono de la clase Persona.
+
+        //Crear gets y sets de cada propiedad.
+
+        //Crear un objeto persona en el main.
+
+        //Utiliza los gets y sets para darle valores a las propiedades edad, nombre y telefono, por último muéstralas por consola.
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Persona persona = new Persona();
+
+        // Uso de Setters - Doy valores
+        persona.setEdad(30);
+        persona.setNombre("Leonardo");
+        persona.setTelefono("209837364");
+
+        // Uso de Getters - Recibo los valores
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getTelefono());
+    }
+}
+
+
+
+
+class Persona {
+
+    //Atributos
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+
+    // Getters and Setters
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Main2.java
@@ -0,0 +1,11 @@
+public class Main2 {
+
+    public static void main(String[] args) {
+
+        int numeros[]={1,2,3,4,5,6,7,8,9};
+
+        for(int i = 0; i < numeros.length; i++ ){
+            System.out.println(numeros[i]);
+        }
+    }
+}
